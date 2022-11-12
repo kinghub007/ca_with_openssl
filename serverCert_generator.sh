1eacbe841963637bf7025f9abed9c99360c0d456
Kingshuk Chowdhury <kingshuk.chowdhury@modirum.com>
2022-11-12 18:10:39 +0200

    RootCA and Intermediate CA generation

--- serverCert_generator.sh
+++ serverCert_generator.sh
@@ -6,3 +6,5 @@ mkdir -p /etc/ssl/ssl_certs/server_cert/private
 
 read -p "Enter FQDN: " FQDN
 /usr/bin/openssl req -out /etc/ssl/ssl_certs/server_cert/csr/"$FQDN".csr.pem -newkey rsa:2048 -nodes -keyout /etc/ssl/ssl_certs/server_cert/private/"$FQDN".key.pem -config /etc/ssl/ssl_certs/config/csr.cnf
+
+/usr/bin/openssl ca -config /etc/ssl/ssl_certs/config/intermCA.cnf -extensions server_cert -days 3750 -notext -md sha512 -in /etc/ssl/ssl_certs/server_cert/csr/"$FQDN".csr.pem -out /etc/ssl/ssl_certs/server_cert/certs/"$FQDN".crt.pem
